ecc6da46c2020c6911d7ab8c74c03417db1f919c
arjunbute10 <arjunbute10@gmail.com>
2023-02-17 13:12:57 +0530

    Assignment complete created three variables of each  data type
    Assignment complete created three variables of each  data type

---
+++ 01_variableBasicsAssign.js
@@ -0,0 +1,12 @@
+var myName ="Arjun"
+console.log("My name :",myName);
+console.log("type Of variable :",typeof myName);
+var mobileNo = 1234567890
+console.log("MY MOBLIE NO :",mobileNo);
+console.log("type of varible :",typeof mobileNo);
+var isStrong = true
+console.log("Is I am srtong :",isStrong);
+console.log("type of varible :",typeof isStrong);
+console.log("********Difference between**********");
+console.log("null:-A null value means that there is no value. It is not equivent to an empty string or 0 it is simply nothing");
+console.log("undefined:- When the varible is declare or not assgined any value then that is undefined");
