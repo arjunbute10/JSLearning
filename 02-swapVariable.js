7d05257cec028face14c97feaf65ad99ed3404f9
arjunbute10 <arjunbute10@gmail.com>
2023-02-17 13:56:56 +0530

    Assignment complete swap two variable value and three variable value
    Assignment complete swap two variable value and three variable value

---
+++ 02-swapVariable.js
@@ -0,0 +1,32 @@
+console.log("________before________");
+var name1 = "Sweety"
+var name2 = "Cutie"
+console.log(name1,name2);
+console.log("________After________");
+var name1 = "Sweety"
+var name2 = "Cutie"
+var temp = name1
+name1=name2
+name2=temp
+console.log(name1,name2);
+console.log("**********Step2**********");
+console.log("________before________");
+var num1 = 100
+var num2 = 200
+var num3 = 300
+console.log(num1,num2,num3);
+console.log("________After________");
+var num1 = 100
+var num2 = 200
+var num3 = 300
+var temp1 =num1
+var temp2 = num2
+var temp3 = num3
+num1=num3
+num2=temp2
+num3=temp1
+console.log(num1,num2,num3);
+
+
+
+
