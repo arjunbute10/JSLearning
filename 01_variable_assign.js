f288b406ecd699db5142563bd7e97f3e2955c1f5
arjunbute10 <arjunbute10@gmail.com>
2023-02-17 13:11:34 +0530

    Updated all details assignment complete
    Updated all details assignment complete

---
+++ 01_variable_assign.js
@@ -0,0 +1,26 @@
+console.log("***************Step 1*******************");
+var myFullName
+console.log(myFullName);
+myFullName="Arjun Bute"
+console.log("My Full Name is",myFullName);
+myFullName="Arjun Anil Bute"
+console.log("*********After Initialization*************");
+console.log("My Full Name is",myFullName);
+console.log("***************Step 2*********************");
+var myLove = "I love only JS"
+console.log(myLove);
+console.log("***************Step 3*********************");
+var firstName="Arjun"
+console.log("My first name is",firstName);
+var lastName ="Bute"
+console.log("My last name is",lastName);
+var age ="23"
+console.log("My age is",age);
+var address ="Ay post Yavatmal"
+console.log("My Address is",address);
+var pincode ="445001"
+console.log("My pincode is",pincode);
+var gender ="Male"
+console.log("Gemder",gender);
+console.log("**********single Line Output**************");
+console.log("My First name",firstName,"My last name",lastName,"Age",age,"pincode",pincode);
