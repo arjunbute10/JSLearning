db239aa90cc8337932737df5512d4b17c9b87cc6
arjunbute10 <arjunbute10@gmail.com>
2023-02-17 13:15:16 +0530

    Assignment Complete create a variable  as per instruction
    Assignment Complete create a variable  as per instruction

---
+++ 02_typeOf.js
@@ -0,0 +1,34 @@
+console.log("############Step.1##############");
+console.log("**************Before initialization***************");
+var bankName
+console.log("Bank Name:-",bankName);
+console.log("*************After initialization*****************");
+bankName = "SBI Bank"
+console.log("Bank Name:-",bankName);
+console.log("Type of varible:-",typeof bankName);
+console.log("#############Step.2##############");
+var mark = '90%'
+console.log("Mark:-",mark);
+console.log("Type of varible:-",typeof mark);
+console.log("###########Step.3#############");
+console.log("**************Before initializatin***************")
+var isWorking
+console.log("Is I am working",isWorking);
+console.log("**************After initializatin***************")
+isWorking = true
+console.log("Is I am working",isWorking);
+console.log("Type of varible:-",typeof isWorking);
+console.log("############Step.4##############");
+var totalCount = "Hundred and seven"
+console.log("Total Count:-",totalCount);
+console.log("Type of varible:-",typeof totalCount);
+console.log("_____Updated variable____");
+var totalCount = 107
+console.log("Total Count:-",totalCount);
+console.log("Type of varible:-",typeof totalCount);
+
+
+
+
+
+
