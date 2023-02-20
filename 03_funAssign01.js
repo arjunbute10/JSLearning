8a7880528ee2159c9ccb78246a8536b134972640
arjunbute10 <arjunbute10@gmail.com>
2023-02-20 19:08:34 +0530

    Assignment complete
    Assignment complete

---
+++ 03_funAssign01.js
@@ -0,0 +1,38 @@
+console.log("_____________NO Arguments & No Return value_______________");
+function bestAnime() {
+    console.log("My favourite anime is Naruto");
+}
+bestAnime();
+function favouriteCharacter() {
+    console.log("My favourite character is Levi Ackerman");
+    
+}
+favouriteCharacter()
+console.log("_____________Assignment No.2_______________");
+function personalDetails(firstName,lastName,collegeName) {
+
+console.log("My first name is:-",firstName,"-My last name is:-",lastName,"-My college name is:-",collegeName);
+    
+}
+personalDetails("Arjun","Bute","S.G.S.P.S Akola");
+
+
+console.log("_____________Assignment No.3_______________");
+function swapValueDude(value1,value2) {
+    var temp = value1
+   value1=value2
+   value2=temp
+    console.log(value1,value2);
+}
+swapValueDude("Virat","Anushaka")
+swapValueDude(1000,2000)
+
+console.log("_____________Assignment No.4_______________");
+function addThreeValue(num1,num2,num3){
+    var sum = num1 + num2 + num3
+return sum;
+}
+var sumResult = addThreeValue(10.23,600,40)  
+console.log(sumResult);
+var sumResult = addThreeValue("Hello ","Good ","Moring ") 
+console.log(sumResult); 
